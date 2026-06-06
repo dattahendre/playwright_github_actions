a3c2b6b521e68b941859403e73e198d224a4a06d
datta walse <dattawalse97@gmail.com>
2026-06-06 22:46:39 +0530

    wellepeople: update tests/wellpeople.spec.js

--- tests/wellpeople.spec.js
+++ tests/wellpeople.spec.js
@@ -6,20 +6,17 @@ test('test', async ({ page }) => {
   await page.getByRole('button', { name: 'Accept All Cookies' }).click();
   await expect(page.getByRole('link', { name: 'Best Sellers' })).toBeVisible();
   //click on home page button best sellers
+
   await page.getByRole('link', { name: 'Best Sellers' }).click();
-  await expect(page.getByRole('link', { name: 'Expressionist Curling Mascara', exact: true })
-).toBeVisible();
-  //await expect(page.getByRole('link', { name: 'Expressionist Curling Mascara' })).toBeVisible();
-  await page.getByRole('button', { name: 'Add to cart' }).first().click();
-  await expect(page.getByRole('button', { name: 'Cart', exact: true })).toBeVisible();
-  
-await page.getByRole('button', { name: 'Checkout', exact: true }).click();
-//verify the checkout page
-await page.getByText('Expressionist Curling Mascara').toString().includes('Expressionist Curling Mascara');
-//verify product price is $20.00
-await page.getByText('$20.00').toString().includes('$20.00');
-//close the browser
-await page.close();
-  
-  
+  await page.getByRole('button', { name: 'Search' }).click();
+  await page.getByRole('searchbox', { name: 'Search' }).click();
+
+  await page.getByRole('searchbox', { name: 'Search' }).click();
+  await page.getByRole('searchbox', { name: 'Search' }).fill('Expressionist Curling Mascara');
+ 
+  await page.locator('#predictive-search-form > svg').click();
+  await expect(page.getByRole('listitem').filter({ hasText: 'expressionist curling mascara' })).toBeVisible();
+  await expect(page.locator('.product-list > product-card > .product-card__figure > .product-card__media').first()).toBeVisible();
+  await page.getByRole('listitem').filter({ hasText: 'expressionist curling mascara' }).click();
+    
 });
