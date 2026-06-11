177a5135c0f308f7dd740192ed093627fdd8d72b
datta walse <dattawalse97@gmail.com>
2026-06-11 15:43:20 +0530

    updated file autocomplete

--- tests/autocomplete.spec.js
+++ tests/autocomplete.spec.js
@@ -4,8 +4,8 @@ test('Keyboard action test case',async({page})=>{
     await page.goto('https://www.google.com/');
     await page.locator("textarea[name='q']").type('Mukesh otwani');
     
-  await page.waitForSelector("//li[@role='presentation']");
-    const suggestions = await page.$$("//li[@role='presentation']");
+  await page.waitForSelector('li[role="presentation"][data-attrid="AutocompletePrediction"]');
+    const suggestions = await page.$$('li[role="presentation"][data-attrid="AutocompletePrediction"]');
    
    for(let i=0;i<suggestions.length;i++){
     const text = await suggestions[i].textContent();
