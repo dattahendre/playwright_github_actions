908ee528c9ba9565a668374ebc7abb0043721018
datta walse <dattawalse97@gmail.com>
2026-06-30 22:47:50 +0530

    updated reporting

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['line'],
-    ['allure-playwright', { outputFolder: 'allure-results' }]
+     ['allure-playwright', { outputFolder: 'allure-results' }],
+     ['html']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
